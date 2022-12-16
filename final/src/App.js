80d5ee038688429b7a0974564ad60be768c81bfb
O'Sullivan <mosullivan22@wustl.edu>
2022-12-16 10:15:02 -0600

    testing react

--- final/src/App.js
+++ final/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import newFact from './newFact';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {};
+        this.deleteB = this.deleteB.bind(this);
+    }
+
+    componentDidMount(){
+
+    }
+
+    deleteB = event =>{
+        console.log(event.target.id);
+    }
+
+    render() {
+        return (
+            <div className="mainSection">
+                <div className="button-area">
+                    <h1>Random Cat & Dog Facts</h1>
+                    <button id="generate-fact-button">Generate a Random Cat Fact</button>
+                    <button id="generate-fact-button">Generate a Random Dog Fact</button>
+                </div>
+                <div className="facts">
+                    <newFact deleteB={this.deleteB}/>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
